7769162c3a0702c0179829ac3a74941a1b7d6f03
Neil Mitchell <ndmitchell@gmail.com>
2018-05-06 00:03:11 +0100

    More work on the travis script

--- travis.hs
+++ travis.hs
@@ -1,7 +1,11 @@
 
 import System.Process.Extra
+import System.Directory
 
 main = do
     system_ "cabal install microlens"
+    createDirectory "Control"
+    writeFile "Control/Lens.hs" "module Control.Lens(module X) where import Lens.Micro as X"
+
     system_ "runhaskell Demo.hs"
     system_ "record-dot-preprocessor --test examples"
