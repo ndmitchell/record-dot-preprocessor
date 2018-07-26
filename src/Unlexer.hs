d46b5d7000df3d62f42c386996d5fba22d7234c4
Neil Mitchell <ndmitchell@gmail.com>
2018-07-26 13:56:33 +0100

    Add LINE droppings to get approximate line numbers correct

--- src/Unlexer.hs
+++ src/Unlexer.hs
@@ -4,8 +4,18 @@
 module Unlexer(unlexer) where
 
 import Lexer
+import Data.List
 
 unlexer :: FilePath -> [Lexeme] -> String
 unlexer src xs =
-    "{-# LINE 1 " ++ show src ++ " #-}\n" ++
-    concat [lexeme ++ whitespace | Lexeme{..} <- xs]
+    dropping 1 ++
+    go 1 True [(line, lexeme ++ whitespace) | Lexeme{..} <- xs]
+    where
+        go :: Int -> Bool -> [(Int, String)] -> String
+        go doc drp ((i, x):xs) =
+            (if doc /= i && i /= 0 && drp then dropping i else "") ++
+            x ++
+            go ((if i == 0 then doc else i) + length (filter (== '\n') x)) ("\n" `isSuffixOf` x) xs
+        go _ _ [] = ""
+
+        dropping n = "{-# LINE " ++ show n ++ " " ++ show src ++ " #-}\n"
