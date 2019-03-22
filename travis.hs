18238114ed3afe344861b3b064f4e8b0cffd5d72
Neil Mitchell <ndmitchell@gmail.com>
2019-03-22 16:09:53 +0000

    Remove the Demo file, no longer useful

--- travis.hs
+++ travis.hs
@@ -4,6 +4,5 @@ import System.Directory
 
 main = do
     system_ "cabal install record-hasfield"
-    system_ "runhaskell Demo.hs"
     system_ "record-dot-preprocessor --test examples"
     system_ "runhaskell -F -pgmF=record-dot-preprocessor -isrc preprocessor/Main.hs"
