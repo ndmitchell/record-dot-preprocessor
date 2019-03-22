16d22f61494de531618f236c2fb5539121a4de64
Neil Mitchell <ndmitchell@gmail.com>
2019-03-22 10:35:27 +0000

    Fix the CI script again

--- travis.hs
+++ travis.hs
@@ -3,7 +3,7 @@ import System.Process.Extra
 import System.Directory
 
 main = do
-    system_ "cabal install record-hasfields"
+    system_ "cabal install record-hasfield"
     system_ "runhaskell Demo.hs"
     system_ "record-dot-preprocessor --test examples"
     system_ "runhaskell -F -pgmF=record-dot-preprocessor -isrc src/Main.hs"
