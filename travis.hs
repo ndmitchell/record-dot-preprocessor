910391fa4acdbbb7f46fd56ea369befa73138978
Neil Mitchell <ndmitchell@gmail.com>
2019-03-22 10:16:46 +0000

    Fix the CI script

--- travis.hs
+++ travis.hs
@@ -3,10 +3,7 @@ import System.Process.Extra
 import System.Directory
 
 main = do
-    system_ "cabal install microlens"
-    createDirectory "Control"
-    writeFile "Control/Lens.hs" "module Control.Lens(module X) where import Lens.Micro as X"
-
+    system_ "cabal install record-hasfields"
     system_ "runhaskell Demo.hs"
     system_ "record-dot-preprocessor --test examples"
     system_ "runhaskell -F -pgmF=record-dot-preprocessor -isrc src/Main.hs"
