60f887d73ea8a219abb0b7bc7a59a8fc7ccc2fac
Neil Mitchell <ndmitchell@gmail.com>
2019-03-22 15:26:45 +0000

    Move preprocessor under preprocessor rather than src

--- travis.hs
+++ travis.hs
@@ -6,4 +6,4 @@ main = do
     system_ "cabal install record-hasfield"
     system_ "runhaskell Demo.hs"
     system_ "record-dot-preprocessor --test examples"
-    system_ "runhaskell -F -pgmF=record-dot-preprocessor -isrc src/Main.hs"
+    system_ "runhaskell -F -pgmF=record-dot-preprocessor -isrc preprocessor/Main.hs"
