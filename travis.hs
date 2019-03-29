7069a32c0fb7d88029314cd13c5a2f7db3dffbd9
Neil Mitchell <ndmitchell@gmail.com>
2019-03-29 08:43:43 +0000

    If the travis test doesn't get success then fail

--- travis.hs
+++ travis.hs
@@ -1,7 +1,12 @@
 
+import Control.Monad
+import Data.List
 import System.Process.Extra
 
 main = do
     system_ "chmod go-w .ghci"
     system_ "chmod go-w ."
-    system_ "ghc -e \":test --installed\""
+    xs <- systemOutput_ "ghc -e \":test --installed\""
+    putStrLn xs
+    unless ("Success" `isInfixOf` xs) $
+        putStrLn "Running the test did not succeed"
