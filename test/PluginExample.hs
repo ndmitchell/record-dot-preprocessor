aae91296ca083a1b793e495f5a24d1dc091a437d
Neil Mitchell <ndmitchell@gmail.com>
2020-08-12 21:42:47 +0100

    Make the plugin example turn on all appropriate warnings

--- test/PluginExample.hs
+++ test/PluginExample.hs
@@ -15,9 +15,11 @@ main = pure ()
 
 #else
 
-{-# OPTIONS_GHC -fplugin=RecordDotPreprocessor -w #-}
+{-# OPTIONS_GHC -fplugin=RecordDotPreprocessor #-}
 {-# LANGUAGE DuplicateRecordFields, TypeApplications, FlexibleContexts, DataKinds, MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
-{-# LANGUAGE PartialTypeSignatures, GADTs, StandaloneDeriving, KindSignatures #-} -- because it's now treated as a comment
+-- things that are now treated as comments
+{-# OPTIONS_GHC -Werror -Wall -Wno-type-defaults -Wno-partial-type-signatures -Wincomplete-record-updates -Wno-unused-top-binds #-}
+{-# LANGUAGE PartialTypeSignatures, GADTs, StandaloneDeriving, KindSignatures #-}
 module PluginExample where
 #include "../examples/Both.hs"
 
