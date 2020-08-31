4b2ecb746027b651a0d8a16fe03ef5598638a7ea
Neil Mitchell <ndmitchell@gmail.com>
2020-08-31 16:47:05 +0100

    #30, disable the incomplete record updates warning

--- test/PluginExample.hs
+++ test/PluginExample.hs
@@ -15,7 +15,7 @@ main = pure ()
 
 #else
 
-{-# OPTIONS_GHC -fplugin=RecordDotPreprocessor #-}
+{-# OPTIONS_GHC -fplugin=RecordDotPreprocessor -Wno-incomplete-record-updates #-}
 {-# LANGUAGE DuplicateRecordFields, TypeApplications, FlexibleContexts, DataKinds, MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 -- things that are now treated as comments
 {-# OPTIONS_GHC -Werror -Wall -Wno-type-defaults -Wno-partial-type-signatures -Wincomplete-record-updates -Wno-unused-top-binds #-}
